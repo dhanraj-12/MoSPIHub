93d89ca02337525b37a1c7f40fe8a2504cafe505
dhanraj-12 <dj004786@gmail.com>
2025-10-31 23:06:19 +0530

    correction in reddis client

--- backend/src/Util/reddisclient.ts
+++ backend/src/Util/reddisclient.ts
@@ -3,7 +3,7 @@ import type {RedisOptions} from "ioredis";
 
 
 const redisConfig: RedisOptions = {
-    host : process.env.REDIS_HOST,
+    host : process.env.REDIS_HOST || "127.0.0.1",
     port : parseInt(process.env.REDIS_PORT || "6379"),
     password : process.env.REDIS_PASSWORD || undefined,
 
