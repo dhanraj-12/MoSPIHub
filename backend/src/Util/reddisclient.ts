3fb5a8ea320c0ec6d17a1897bc8c05374733a9a0
dhanraj-12 <dj004786@gmail.com>
2025-11-06 23:36:39 +0530

    made changes in redisclietn

--- backend/src/Util/reddisclient.ts
+++ backend/src/Util/reddisclient.ts
@@ -3,9 +3,9 @@ import type {RedisOptions} from "ioredis";
 
 
 const redisConfig: RedisOptions = {
-    host : process.env.REDIS_HOST || "127.0.0.1",
-    port : parseInt(process.env.REDIS_PORT || "6379"),
-    password : process.env.REDIS_PASSWORD || undefined,
+    host : process.env.REDIS_HOST,
+    port : parseInt(process.env.REDIS_PORT),
+    password : process.env.REDIS_PASSWORD,
 
     maxRetriesPerRequest: null,
     enableReadyCheck: true,
